2fe43e10cb450b20ed50c3bc451197934e85a4ad
Shaker Kattoula <smkattoula@gmail.com>
2020-09-11 12:34:20 -0700

    setup users route for registering a new user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
-// Route:          GET api/users
-// Description:    Test route
+// Route:          POST api/users
+// Description:    Register a new user
 // Access:         Public
-router.get("/", (req, res) => {
-  res.send("User Route");
-});
+router.post(
+  "/",
+  [
+    check("name", "Please enter a name").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
 
 module.exports = router;
