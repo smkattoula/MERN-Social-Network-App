d93d76e3d4eed5bd7bdfaa7949498acaa801d33a
Shaker Kattoula <smkattoula@gmail.com>
2020-09-15 18:25:45 -0700

    setup POST request to add a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,11 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
 
-// Route:          GET api/posts
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// Route:          POST api/posts
 // Description:    Test route
-// Access:         Public
-router.get("/", (req, res) => {
-  res.send("Post Route");
-});
+// Access:         Private
+router.post(
+  "/",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select("-password");
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
