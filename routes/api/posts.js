57caaa210173cf3af20dee85c081afe00851b919
Shaker Kattoula <smkattoula@gmail.com>
2020-09-15 18:53:51 -0700

    setup GET request to fetch all posts and a single post by ID, setup DELETE request to delete a single post by ID

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -39,4 +39,66 @@ router.post(
   }
 );
 
+// Route:          GET api/posts
+// Description:    Get all posts
+// Access:         Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+// Route:          GET api/posts/:id
+// Description:    Get a single post by ID
+// Access:         Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
+// Route:          DELETE api/posts/:id
+// Description:    Delete a post
+// Access:         Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    // Check user
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+    await post.remove();
+
+    res.json({ msg: "Post removed" });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
