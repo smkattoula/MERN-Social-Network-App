38a545b2ae208905faa1b0642e3f130bf1fe37c7
Shaker Kattoula <smkattoula@gmail.com>
2020-09-23 16:55:18 -0700

    implement login form with state

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+// import axios from "axios";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Success!");
+    //   const newUser = {
+    //     name,
+    //     email,
+    //     password,
+    //   };
+    //   try {
+    //     const config = {
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //     };
+
+    //     const body = JSON.stringify(newUser);
+
+    //     const res = await axios.post("/api/users", body, config);
+    //     console.log(res.data);
+    //   } catch (err) {
+    //     console.error(err.response.data);
+    //   }
+  };
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Login</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign Into Your Account
+      </p>
+      <form onSubmit={(e) => onSubmit(e)} className="form">
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            minLength="6"
+            name="password"
+            value={password}
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+        <input type="submit" value="Login" className="btn btn-primary" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Register</Link>
+      </p>
+    </Fragment>
+  );
 };
 
 export default Login;
