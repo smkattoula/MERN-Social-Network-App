fef687c30049821887a0677f13ef01f7b6039e6c
Shaker Kattoula <smkattoula@gmail.com>
2020-09-24 11:50:48 -0700

    set up Alert reducer, action, types, component, and action call

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,8 +1,12 @@
 import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
+
 // import axios from "axios";
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -18,7 +22,7 @@ const Register = () => {
   const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("Passwords do not match");
+      setAlert("Passwords do not match", "danger");
     } else {
       console.log("Success!");
       //   const newUser = {
@@ -103,4 +107,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.protoTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert })(Register);
