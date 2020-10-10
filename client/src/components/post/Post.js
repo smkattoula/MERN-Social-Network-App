46dc67a7112b14d34fe22a1dcc2e0436bbbcaf81
Shaker Kattoula <smkattoula@gmail.com>
2020-10-10 16:14:25 -0700

    display comments on a post and add delete comment functionality

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,12 +5,13 @@ import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
 import PostItem from "../posts/PostItem";
 import CommentForm from "../post/CommentForm";
+import CommentItem from "../post/CommentItem";
 import { getPost } from "../../actions/post";
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
     getPost(match.params.id);
-  }, [match.params.id]);
+  }, [getPost, match.params.id]);
 
   return loading || post === null ? (
     <Spinner />
@@ -21,6 +22,17 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
       </Link>
       <PostItem post={post} showActions={false} />
       <CommentForm postId={post._id} />
+      <div className="comments">
+        {post.comments.map((comment) => {
+          return (
+            <CommentItem
+              key={comment._id}
+              comment={comment}
+              postId={post._id}
+            />
+          );
+        })}
+      </div>
     </Fragment>
   );
 };
